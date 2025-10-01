dce6f19045361d1de511fcace0f019be67aaa853
mrqsdf <maqw@free.fr>
2025-10-01 15:27:41 +0200

    refactor Main

--- src/main/java/fr/mrqsdf/vastia4j/Main.java
+++ src/main/java/fr/mrqsdf/vastia4j/Main.java
@@ -1,4 +1,4 @@
-package fr.mrqsdf;
+package fr.mrqsdf.vastia4j;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
